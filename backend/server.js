724bc34fc665bf54eab023edd8c5f6148162c9c5
Alex Romo III <alexromo3rd@gmail.com>
2021-05-30 16:08:59 -0500

    Fixed?

--- backend/server.js
+++ backend/server.js
@@ -61,7 +61,7 @@ app.post('/api/products', createProduct);
 app.post('/api/payment', submitPayment);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
