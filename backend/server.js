579bc430caf88a1d2d413110ad1fbc380e17c20f
Alex Romo III <alexromo3rd@gmail.com>
2021-05-30 15:03:13 -0500

    Using cookie-session instead of express-session due to memory leak in production

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const massive = require('massive');
-const session = require('express-session');
+const session = require('cookie-session');
 const path = require('path');
 const {
   getAllProducts,
@@ -28,8 +28,6 @@ app.use(express.json());
 app.use(
   session({
     secret: SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 365 },
   })
 );
